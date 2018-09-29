a388c4c71aee5df14172117c4abeb1a4bfbd8dfb
niyasrahman <[niyas@techarr.com]>
2018-09-29 17:52:15 +0530

    changws

--- src/components/mainbanner.js
+++ src/components/mainbanner.js
@@ -5,7 +5,7 @@ class Banner extends Component {
   render() {
     return (
       <div className="news-container">
-        <div className="news_tag">Latest</div>
+        <div className="news_tag">Latest</div> 
         <img className="news_image" src={this.props.imageURL} />
         <h3 className="news_text">{this.props.headline}</h3>
       </div>
