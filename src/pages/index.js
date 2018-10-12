f556ef6220f2eb7c1f42fdd895db689160faecf9
niyasrahman <[niyas@techarr.com]>
2018-10-12 12:58:39 +0530

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <Banner
     	imageURL="http://a3.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1160867_1296x729.jpg&w=920&h=518&scale=crop&cquality=40&location=origin"
-		headline="To deploy to a different project than the one specified in the firebase.json, use the project key in your .travis.yml:"
+		headline="blablablablablablablablablablablablablabla"
     />
   </Layout>
 )
